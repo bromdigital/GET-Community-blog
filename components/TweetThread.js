f064283b8dff9425199db4b1bf8601435fcf7c3f
Simon Bromfield <simon@simonbromfield>
2022-10-17 22:10:23 +0100

    updated twitter thread card

--- components/TweetThread.js
+++ components/TweetThread.js
@@ -4,7 +4,11 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material'
+import {
+  Grid,
+  CardActionArea,
+  CardMedia
+} from '@mui/material'
 
 export default function TwitterThread({ post }) {
   return (
@@ -13,20 +17,41 @@ export default function TwitterThread({ post }) {
       sm={6}
       lg={4}
     >
-    <Card sx={{border: "5px solid #4A99E9"}}>
-        <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-            {`Tweet - ${post.frontmatter.title}`}
-        </Typography>
-      </CardContent>
-      <CardActions>
-          <Button
-            size="small"
-            href={post.frontmatter.url}
-            target="_blank"
-          >View Tweet</Button>
-      </CardActions>
-    </Card>
+      <Card
+        sx={{
+          backgroundColor: "#4A99E9",
+          height: "100%"
+        }}>
+      <CardActionArea
+            LinkComponent="a"
+          href={post.frontmatter.url}
+          target="_blank"
+          sx={{
+            height: "100%"
+          }}>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div"
+            sx={{
+              color: "#FFF",
+              fontWeight: "bold"
+            }}>
+              Twitter Thread
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div"
+            sx={{
+              color: "#FFF"
+            }}>
+              {post.frontmatter.title}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div"
+            sx={{
+              color: "#FFF"
+            }}>
+              - {post.frontmatter.postedBy}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Grid>
   );
 }
