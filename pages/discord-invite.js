b9eaabcc06b76337961562cf055d8778a2e9db9c
Simon Bromfield <simon@simonbromfield>
2022-11-16 19:32:26 +0000

    created discord-invite route

---
+++ pages/discord-invite.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { Container } from '@mui/system'
+
+export default function Home() {
+
+  return (
+    <div>
+      <Head>
+        <title>Discord Invite</title>
+      </Head>
+      <Container id="resources" maxWidth={false}>
+      <h1>Discord Invite</h1>
+      </Container>
+    </div>
+  )
+}
