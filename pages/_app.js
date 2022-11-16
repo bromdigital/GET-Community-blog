78d6b7a7e11a768c0e26a67b97a6666313492269
Simon Bromfield <simon@simonbromfield>
2022-11-16 22:44:31 +0000

    discord invite

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,11 @@ import * as React from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../styles/globals.css'
-import SEO from '../components/seo'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header>
-        <SEO />
       </Header>
       <Component {...pageProps} />
       <Footer />
